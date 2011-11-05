e08d005095c0efd9886d9a8e1f80c3c8254ef437
Sam Kimbrel <kimbrel@me.com>
2011-11-05 11:22:52 -0700

    Working Menger-sponge generator

--- menger.py
+++ menger.py
@@ -1,67 +1,70 @@
 import argparse
 
-CUBE = "cube(size=10);\n"
-SCALE_FACTOR = 1.0 / 3.0
+CUBE = "cube(size=1);\n"
+SCALE_FACTOR = 3
 TRANSLATIONS = (
-	(1,1,1),
-	(1,1,0),
-	(2,1,1),
-	(0,1,1),
-	(1,1,2),
-	(1,0,1),
-	(1,2,1),
-	)
+(0, 0, 0),
+(0, 0, 1),
+(0, 0, 2),
+(0, 1, 0),
+(0, 1, 2),
+(0, 2, 0),
+(0, 2, 1),
+(0, 2, 2),
+(1, 0, 0),
+(1, 0, 2),
+(1, 2, 0),
+(1, 2, 2),
+(2, 0, 0),
+(2, 0, 1),
+(2, 0, 2),
+(2, 1, 0),
+(2, 1, 2),
+(2, 2, 0),
+(2, 2, 1),
+(2, 2, 2),
+)
 
-def scale(factor, inner):
-	return "scale([%(factor)s/3, %(factor)s/3, %(factor)s/3]) {\n%(inner)s}\n" % {'factor': factor, 'inner': inner}
 
 def translate(x, y, z, inner):
-	return "translate([%(x)s, %(y)s, %(z)s]) {\n%(inner)s}\n" % {
-		'x': x,
-		'y': y,
-		'z': z,
-		'inner': inner
-	}
-	
-def union(inner):
-	return "union() {\n%(inner)s}\n" % { 'inner': inner }
+    return "translate([%(x)s, %(y)s, %(z)s]) {\n%(inner)s}\n" % {
+        'x': x,
+        'y': y,
+        'z': z,
+        'inner': inner}
 
-def diff(inner):
-	return "difference() {\n%(inner)s}\n" % { 'inner': inner }
-	
-def generate(remainingIterations, op):
-	if remainingIterations == 0:
-		return CUBE
-	else:
-		if op == 'UNION':
-			nextOp = diff
-			op = union
-		else:
-			nextOp = union
-			op = diff
-		next = generate(remainingIterations - 1, nextOp)
-		children = [ scale(SCALE_FACTOR, translate(x, y, z, next)) for x, y, z in TRANSLATIONS ]
-		inner = ''.join(children)
-		return op(scale(SCALE_FACTOR, inner))
+
+def union(inner):
+    return "union() {\n%(inner)s}\n" % {'inner': inner}
+
+
+def generate(remainingIterations):
+    if remainingIterations == 0:
+        return CUBE
+    else:
+        next = generate(remainingIterations - 1)
+        children = [translate(
+            SCALE_FACTOR ** (remainingIterations - 1) * x,
+            SCALE_FACTOR ** (remainingIterations - 1) * y,
+            SCALE_FACTOR ** (remainingIterations - 1) * z,
+        next) for x, y, z in TRANSLATIONS]
+        inner = ''.join(children)
+        return union(inner)
+
 
 def printSponge(iterations):
-	output = generate(iterations, 'union')
-	print output
+    output = generate(iterations)
+    print output
 
-def main():
-	parser = argparse.ArgumentParser(description="Make a Menger sponge for OpenSCAD.")
-	parser.add_argument('n')
-	args = parser.parse_args()
-	try:
-		iters = int(args.n)
-	except AttributeError, ValueError:
-		iters = 1
-	printSponge(iters)
-	
+
 if __name__ == '__main__':
-	main()
-
-	
-	
-		
-	
+    parser = argparse.ArgumentParser(
+        description="Make a Menger sponge for OpenSCAD.")
+    parser.add_argument('n')
+    values = parser.parse_args()
+    try:
+        iters = int(values.n)
+        printSponge(iters)
+    except AttributeError, ValueError:
+        iters = 1
+        printSponge(iters)
