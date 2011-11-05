b80840fcd3f297bc9a263291680cfe29e63b582f
Sam Kimbrel <kimbrel@me.com>
2011-11-05 12:18:01 -0700

    Use itertools to generate our translations. And document the magic.

--- menger.py
+++ menger.py
@@ -1,34 +1,31 @@
 """menger.py - generate OpenSCAD-compatible descriptions of Menger sponges
 Takes a single argument n, an integer number of iterations to run.
 Prints OpenSCAD code to stdout.
+
+My first attempt at this was to try constructing the sponge exactly the way
+the mathematical definition works, i.e. to take the largest cube and
+iteratively remove pieces of it. This led to having to track a lot more state
+at each step of the recursion and was generally gross.
+
+Instead, this relies on the realization that since we're only ever going
+to compute a finite approximation of the fractal, we can build it up from
+the smallest piece by constructing each iteration as a union of the next-
+smallest iteration. Hurray for approximation.
 """
 
 import argparse
+import itertools
 
 CUBE = "cube(size=1);\n"
 SCALE_FACTOR = 3
-TRANSLATIONS = (
-(0, 0, 0),
-(0, 0, 1),
-(0, 0, 2),
-(0, 1, 0),
-(0, 1, 2),
-(0, 2, 0),
-(0, 2, 1),
-(0, 2, 2),
-(1, 0, 0),
-(1, 0, 2),
-(1, 2, 0),
-(1, 2, 2),
-(2, 0, 0),
-(2, 0, 1),
-(2, 0, 2),
-(2, 1, 0),
-(2, 1, 2),
-(2, 2, 0),
-(2, 2, 1),
-(2, 2, 2),
-)
+# The list of places we're going to translate our current step to.
+# Since an iteration of the sponge is carried out by removing the small cube
+# at the very center of the large cube and the six cubes in the center of its
+# faces, we want to instead build our sponge by repeating a smaller block
+# in all locations *except* those seven points. Luckily, what those points have
+# in common (in our vector space) is two or more coordinates equal to 1.
+TRANSLATIONS = [vector for vector in itertools.product((0, 1, 2), repeat=3)
+    if vector.count(1) < 2]
 
 
 def translate(x, y, z, inner):
